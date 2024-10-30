de113d89c901b2294bf169bc62b9bbf6105b56b2
Tornike Khatiashvili <95305738+Tornike1123@users.noreply.github.com>
2024-10-30 15:10:25 +0400

    Internal modificator

--- AccessModificator/ProtectedModificator.cs
+++ AccessModificator/ProtectedModificator.cs
@@ -1,35 +1,35 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Threading.Tasks;
 
-namespace OOP.AccessModificator
-{
-    class ProtectedModificator // Base class (მშობელი კლასი)
-    {
-        protected int number;
-        protected void display(){
-            Console.WriteLine("Protected method in BaseClass.");
-        }
-    }
+// namespace OOP.AccessModificator
+// {
+//     class ProtectedModificator // Base class (მშობელი კლასი)
+//     {
+//         protected int number;
+//         protected void display(){
+//             Console.WriteLine("Protected method in BaseClass.");
+//         }
+//     }
 
-    class DerivedClass : ProtectedModificator
-    {
-        public void showDisplay()
-        {
-             // number ცვლადზე და Display() მეთოდზე წვდომა გვაქვს, რადგან ისინი protected არიან
-            number = 10;
-            display();
-            Console.WriteLine("number:" + number);
-        }
-    }
+//     class DerivedClass : ProtectedModificator
+//     {
+//         public void showDisplay()
+//         {
+//              // number ცვლადზე და Display() მეთოდზე წვდომა გვაქვს, რადგან ისინი protected არიან
+//             number = 10;
+//             display();
+//             Console.WriteLine("number:" + number);
+//         }
+//     }
 
-     class Program
-    {
-        static void Main(string[] args)
-        {
-            DerivedClass derived = new DerivedClass();
-            derived.showDisplay(); // გამოიძახებს display() მეთოდს და number ცვლადს
-        }
-    }
-}
+//      class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             DerivedClass derived = new DerivedClass();
+//             derived.showDisplay(); // გამოიძახებს display() მეთოდს და number ცვლადს
+//         }
+//     }
+// }
--- obj/Debug/net8.0/OOP.AssemblyInfo.cs
+++ obj/Debug/net8.0/OOP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2288b7610e16e82edfdd88ccc99766b2ad23f1f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64ef151cc4747f7174191a572455a4bd3c1b4eb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
